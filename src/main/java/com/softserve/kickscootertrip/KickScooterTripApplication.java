ab0c91b0056a336c1e956920721f2a2199fe478d
OlegBenuk <benukoleg@gmail.com>
2020-03-12 17:21:28 +0200

    Hello trip app

---
+++ src/main/java/com/softserve/kickscootertrip/KickScooterTripApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.kickscootertrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickScooterTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickScooterTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softserve/kickscootertrip/KickScooterTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.kickscootertrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickScooterTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
